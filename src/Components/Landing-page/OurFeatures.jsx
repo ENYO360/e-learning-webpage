87a42eae223801f19e6ba99aaf834984c31ccac1
Shaibu Moses <106714361+ENYO360@users.noreply.github.com>
2024-10-20 22:05:00 +0100

    Still working on the Landing Page

--- src/Components/Landing-page/OurFeatures.jsx
+++ src/Components/Landing-page/OurFeatures.jsx
@@ -1,5 +1,5 @@
 // This is the contents for OUR FEATURES section of the Landing page
-import GroupImage from '../../Images/landing-page/our-features-group-image.png'
+import GroupImage from '../../Images/landing-page/our-features-group-images.png'
 import ToolsForTeachers from '../../Images/landing-page/tools-for-teachers.png'
 import TrueOrFalse from '../../Images/landing-page/true-or-false.png'
 import ClassManagement from '../../Images/landing-page/class-management.png'
@@ -8,61 +8,71 @@ export default function OurFeatures() {
     return (
         <>
             <div>
-                <h2>Our Features</h2>
-                <p>This very extraordinary feature, can make learning 
+                <h2 className='our-features'>Our <span>Features</span></h2>
+                <p className='our-features-pgh'>This very extraordinary feature, can make learning
                     activities more efficient
                 </p>
             </div>
-            <div>
-                <img src={GroupImage} alt="group of people" />
+            <div className="our-features-group2">
+                <div>
+                    <img src={GroupImage} alt="group of people" width='800' />
+                </div>
+                <div className='a-user'>
+                    <h2>A <span>user interface</span> designed for the classroom</h2>
+                    <ul>
+                        <li className='item1'>
+                            Teachers don’t get lost in the grid view and have a dedicated Podium space.
+                        </li>
+                        <li className='item2'>
+                            TA’s and presenters can be moved to the front of the class.
+                        </li>
+                        <li className='item3'>
+                            Teachers can easily see all students and class data at one time.
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h2>A user interface designed for the classroom</h2>
-                <ul>
-                    <li>
-                        Teachers don’t get lost in the grid view and have a dedicated Podium space.
-                    </li>
-                    <li>
-                        TA’s and presenters can be moved to the front of the class. 
-                    </li>
-                    <li>
-                        Teachers can easily see all students and class data at one time.
-                    </li>
-                </ul>
+            <div className="our-features-group3">
+                <div className='tools'>
+                    <h2><span>Tools</span> For Teachers And Learners</h2>
+                    <p>Class has a dynamic set of teaching tools built to be deployed and used during class.
+                        Teachers can handout assignments in real-time for students to complete and submit.
+                    </p>
+                </div>
+                <div>
+                    <img src={ToolsForTeachers} alt="Student's picture" />
+                </div>
             </div>
-            <div>
-                <h2>Tools For Teachers And Learners</h2>
-                <p>Class has a dynamic set of teaching tools built to be deployed and used during class.
-                    Teachers can handout assignments in real-time for students to complete and submit.
-                </p>
+            <div className="our-features-group4">
+                <div>
+                    <img src={TrueOrFalse} alt="city by the river bank" />
+                </div>
+                <div className='assessments'>
+                    <h2>Assessments, <span>Quizes</span>, Test</h2>
+                    <p>Easily launch live assignments, quizzes, and tests.
+                        Student results are automatically entered in the online gradebook.
+                    </p>
+                </div>
             </div>
-            <div>
-                <img src={ToolsForTeachers} alt="Student's picture" />
+            <div className="class-mgt">
+                <div>
+                    <h2><span>Class Management</span> Tools for Educators</h2>
+                    <p>Class provides tools to help run and manage the class such as Class Roster, Attendance, and more.
+                        With the Gradebook, teachers can review and grade tests and quizzes in real-time.
+                    </p>
+                </div>
+                <div className='class-mgt-img'>
+                    <img src={ClassManagement} alt="group of Teachers" width='650' />
+                </div>
             </div>
-            <div>
-                <img src={TrueOrFalse} alt="city by the river bank" />
-            </div>
-            <div>
-                <h2>Assessments, Quizes, Test</h2>
-                <p>Easily launch live assignments, quizzes, and tests.
-                    Student results are automatically entered in the online gradebook.
-                </p>
-            </div>
-            <div>
-                <h2>Class Management Tools for Educators</h2>
-                <p>Class provides tools to help run and manage the class such as Class Roster, Attendance, and more. 
-                    With the Gradebook, teachers can review and grade tests and quizzes in real-time.
-                </p>
-            </div>
-            <div>
-                <img src={ClassManagement} alt="group of Teachers" />
-            </div>
-            <div>
-                <img src={OneOnOne} alt="two teachers" />
-            </div>
-            <div>
-                <h2>One-on-One Discussions</h2>
-                <p>Teachers and teacher assistants can talk with students privately without leaving the Zoom environment.</p>
+            <div className="one-on-one">
+                <div>
+                    <img src={OneOnOne} alt="two teachers" />
+                </div>
+                <div>
+                    <h2>One-on-One Discussions</h2>
+                    <p>Teachers and teacher assistants can talk with students privately without leaving the Zoom environment.</p>
+                </div>
             </div>
             <button>See more features</button>
         </>
--- src/Components/Landing-page/WhatIsTOTC.jsx
+++ src/Components/Landing-page/WhatIsTOTC.jsx
@@ -1,7 +1,5 @@
 // This is the contents for what is TOTC? section of the Landing page
-import ForInstructors from '../../Images/landing-page/for-instructor.png'
-import ForStudents from '../../Images/landing-page/for-students.png'
-import YouCan from '../../Images/landing-page/you-can-do-with-TOTC.png'
+import YouCan from '../../Images/landing-page/you-can-do.png'
 
 export default function WhatIsTOTC() {
     return (
@@ -9,38 +7,40 @@ export default function WhatIsTOTC() {
             <div className='whatsTOTC-container'>
                 <div className='what-is-totc'>
                     <h2 className='what'>What is <span>TOTC?</span></h2>
-                    <p className='what-paragraph'>TOTC is a platform that allows educators to create online 
-                        classes whereby they can store the course materials online; 
-                        manage assignments, quizzes and exams; monitor due dates; grade 
+                    <p className='what-paragraph'>TOTC is a platform that allows educators to create online
+                        classes whereby they can store the course materials online;
+                        manage assignments, quizzes and exams; monitor due dates; grade
                         results and provide students with feedback all in one place.
                     </p>
                 </div>
                 <div className='instructor-students'>
-                <div className='for-instructors'>
-                    <div>
-                        <h2>FOR INSTRUCTORS</h2>
-                        <button>Start a class today</button>
+                    <div className='for-instructors'>
+                        <div>
+                            <h2>FOR INSTRUCTORS</h2>
+                            <button>Start a class today</button>
+                        </div>
+                    </div>
+                    <div className='for-students'>
+                        <div>
+                            <h2>FOR STUDENTS</h2>
+                            <button>Enter access code</button>
+                        </div>
                     </div>
                 </div>
-                <div className='for-students'>
-                    <div>
-                        <h2>FOR STUDENTS</h2>
-                        <button>Enter access code</button>
-                    </div>
-                </div>
-                </div>
-                <div>
-                    <h3>Everything you can do in a physical classroom, you can do with TOTC</h3>
-                    <p>TOTC’s school management software helps traditional and online schools manage 
-                        scheduling, attendance, payments and virtual classrooms all in one secure 
+                <div className="everything">
+                <div className='everything-you-can'>
+                    <h3>Everything you can do in a physical classroom, <span>you can do with TOTC</span></h3>
+                    <p>TOTC’s school management software helps traditional and online schools manage
+                        scheduling, attendance, payments and virtual classrooms all in one secure
                         cloud-based system.
                     </p>
-                    <button>Learn more</button>
+                    <a href="#">Learn more</a>
                 </div>
                 <div>
-                    <img src={YouCan} alt="Students attending lectures" />
+                    <img src={YouCan} alt="Students attending lectures" width='650'/>
+                </div>
                 </div>
             </div>
         </>
-    ) 
+    )
 }
